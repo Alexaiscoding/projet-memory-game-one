f60869358941c6264e0591c8322be11988469bc9
Alexaiscoding <93916126+Alexaiscoding@users.noreply.github.com>
2021-12-16 15:33:04 +0100

    add one

---
+++ indexdom.js
@@ -0,0 +1,74 @@
+//class MemoryGame{
+
+
+//3-Flip card 
+const cards = document.querySelectorAll('.memory-card');
+
+
+    function flipCard() {
+      this.classList.toggle('flip');
+
+
+
+    }
+
+    cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+// - Match Cards
+
+//for ...
+
+//function checkForMatch()
+//{
+ //   let matchCard = firstCard.dataset.framework === secondCard.dataset.framework;
+
+
+//}
+
+//desactivate the card + unflipcard function a definir
+
+//When select two cards verify if the same, if match declare win/true
+//
+
+function matchChecked (card1,card2) {
+let firstOpencard = card1 => { documentElementSelector ('div.back-face',.src)} // +le stocker
+let secondOpenCard= card2 = 
+this.clickedCards=0;
+   this.clickedCards ++
+     if (card1 === card2){
+       return true;}
+    else{
+         return false;}
+   }
+    
+
+
+
+
+//Verify is the game is Finished() {
+    
+ //   finishedCheck(){
+ //   if ( this.guessCards === this.cards.length/2){
+ //         return true; }
+ //     else {
+ //         return false; }
+ //   }
+    
+
+//function restart
+
+
+
+
+// constructor (cards) {
+
+ //   this.cards=cards;
+ //   this.openCards = [];
+  //  this.clickedCards=0;
+    //this.GuessCards=0;
+    //
+//shuffleCards?
+// A FAIRE J+3
+//*function reset 
+
